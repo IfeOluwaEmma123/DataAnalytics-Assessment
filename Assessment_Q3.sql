77e0c6da71888e01cac6f7ba2b248b347e87e904
IfeOluwa Ayoola <78968110+IfeOluwaEmma123@users.noreply.github.com>
2025-05-20 09:45:34 +0100

    Update Assessment_Q3.sql

--- Assessment_Q3.sql
+++ Assessment_Q3.sql
@@ -10,6 +10,5 @@ SELECT
 FROM plans_plan p
 LEFT JOIN savings_savingsaccount s
   ON s.plan_id = p.id
--- WHERE p.active = 1   <-- remove or comment out this line
 GROUP BY p.id, p.owner_id, p.is_regular_savings
 HAVING MAX(s.transaction_date) < DATE_SUB(CURDATE(), INTERVAL 365 DAY);
